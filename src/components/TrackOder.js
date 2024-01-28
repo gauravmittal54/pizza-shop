3a6ccc78095d778487ef7f2e42d741702be3f14a
Gaurav Mittal <61792468+gauravmittal54@users.noreply.github.com>
2024-01-28 14:00:40 +0530

    Add files via upload

---
+++ src/components/TrackOder.js
@@ -0,0 +1,13 @@
+// TrackOrder.js
+import React from 'react';
+
+const TrackOrder = () => {
+  return (
+    <div>
+      <h2>Track Order</h2>
+      <p>Track the status of your order here.</p>
+    </div>
+  );
+};
+
+export default TrackOrder;
